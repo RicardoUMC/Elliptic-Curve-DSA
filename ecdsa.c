a7fdfa62a73c4f91529f2568f0bd04c3189dec15
Ricardo Mora <rurimoca@gmail.com>
2024-10-09 03:18:09 -0600

    Menu de usuario

--- ecdsa.c
+++ ecdsa.c
@@ -1,51 +1,83 @@
 #include "../ecop/EC.h"
 #include <stdio.h>
-#include <stdbool.h>
-#include <gmp.h>
 #include <openssl/rand.h>
 
 void configure_public_params(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G);
-void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G, char *filename);
-void ecdsa_signature(mpz_t r, mpz_t s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G);
-bool ecdsa_verification(mpz_t r, mpz_t s, mpz_t m, char *filename);
+void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G, char *filename);
+void ecdsa_signature(mpz_t *r, mpz_t *s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G);
+bool ecdsa_verification(mpz_t *r, mpz_t *s, mpz_t m, char *filename);
 
 int main(void) {
     mpz_t p, a, b, q, d, m, r, s;
     ec_point G;
+    int option;
+    bool keys_generated = false, signature_generated = false;
 
     mpz_inits(p, a, b, q, d, m, r, s, G.x, G.y, NULL);
 
-    configure_public_params(p, a, b, q, &G);
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Generate Keys\n");
+        printf("2. Sign Message\n");
+        printf("3. Verify Signature\n");
+        printf("4. Exit\n");
+        printf("Select an option: ");
+        scanf("%d", &option);
 
-    char filename[] = "ecdsa_keypair.txt";
-    generate_key_pair(p, a, b, q, G, filename);
+        switch (option) {
+            case 1:
+                configure_public_params(p, a, b, q, &G);
+                generate_key_pair(p, a, b, q, &G, "ecdsa_keypair.txt");
+                keys_generated = true;
+                break;
+            case 2:
+                if (!keys_generated) {
+                    printf("Please generate keys first.\n");
+                    break;
+                }
+                printf("Enter the private key d: ");
+                mpz_inp_str(d, stdin, 10);
 
-    printf("Enter the private key d: ");
-    mpz_inp_str(d, stdin, 10);
+                printf("Enter a message m: ");
+                mpz_inp_str(m, stdin, 10);
 
-    printf("Enter a message m: ");
-    mpz_inp_str(m, stdin, 10);
+                if (mpz_cmp_ui(m, 0) <= 0 || mpz_cmp(m, q) >= 0) {
+                    fprintf(stderr, "Error: m must be in the range 0 < m < q\n");
+                    break;
+                }
 
-    if (mpz_cmp_ui(m, 0) <= 0 || mpz_cmp(m, q) >= 0) {
-        fprintf(stderr, "Error: m must be in the range 0 < m < q\n");
-        mpz_clears(p, a, b, q, d, m, r, s, G.x, G.y, NULL);
-        return 1;
+                ecdsa_signature(&r, &s, d, m, p, a, b, q, &G);
+                gmp_printf("Signature: (r, s) = (%Zd, %Zd)\n", r, s);
+                signature_generated = true;
+                break;
+            case 3:
+                if (!signature_generated) {
+                    printf("Please sign a message first.\n");
+                    break;
+                }
+
+                printf("Enter the message m to verify: ");
+                mpz_inp_str(m, stdin, 10);
+
+                if (mpz_cmp_ui(m, 0) <= 0 || mpz_cmp(m, q) >= 0) {
+                    fprintf(stderr, "Error: m must be in the range 0 < m < q\n");
+                    break;
+                }
+
+                ecdsa_verification(&r, &s, m, "ecdsa_keypair.txt") ?
+                    printf("The signature is valid.\n") :
+                    printf("The signature is not valid.\n");
+
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                mpz_clears(p, a, b, q, d, m, r, s, G.x, G.y, NULL);
+                return 0;
+            default:
+                printf("Invalid option. Please select again.\n");
+                break;
+        }
     }
-
-    ecdsa_signature(r, s, d, m, p, a, b, q, G);
-
-    gmp_printf("Signature: (r, s) = (%Zd, %Zd)\n", r, s);
-
-    bool is_valid = ecdsa_verification(r, s, m, filename);
-    if (is_valid) {
-        printf("The signature is valid.\n");
-    } else {
-        printf("The signature is not valid.\n");
-    }
-
-    mpz_clears(p, a, b, q, d, m, r, s, G.x, G.y, NULL);
-
-    return 0;
 }
 
 void configure_public_params(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G) {
@@ -69,7 +101,7 @@ void configure_public_params(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G) {
     G->z = 1;
 }
 
-void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G, char *filename) {
+void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G, char *filename) {
     mpz_t d;
     ec_point B;
 
@@ -95,11 +127,11 @@ void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G, char *fil
     }
 
     if (gmp_fprintf(file, "p = %Zd, ", p) < 0 ||
-        gmp_fprintf(file, "a = %Zd, ", a) < 0 ||
-        gmp_fprintf(file, "b = %Zd, ", b) < 0 ||
-        gmp_fprintf(file, "q = %Zd, ", q) < 0 ||
-        gmp_fprintf(file, "G = (%Zd:%Zd:%d), ", G.x, G.y, G.z) < 0 ||
-        gmp_fprintf(file, "B = (%Zd:%Zd:%d)\n", B.x, B.y, B.z) < 0) {
+            gmp_fprintf(file, "a = %Zd, ", a) < 0 ||
+            gmp_fprintf(file, "b = %Zd, ", b) < 0 ||
+            gmp_fprintf(file, "q = %Zd, ", q) < 0 ||
+            gmp_fprintf(file, "G = (%Zd:%Zd:%d), ", G->x, G->y, G->z) < 0 ||
+            gmp_fprintf(file, "B = (%Zd:%Zd:%d)\n", B.x, B.y, B.z) < 0) {
         perror("Error writing to file\n");
     }
 
@@ -111,7 +143,7 @@ void generate_key_pair(mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G, char *fil
     mpz_clears(d, B.x, B.y, NULL);
 }
 
-void ecdsa_signature(mpz_t r, mpz_t s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point G) {
+void ecdsa_signature(mpz_t *r, mpz_t *s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t b, mpz_t q, ec_point *G) {
     mpz_t k, k_inv, temp;
     ec_point R;
 
@@ -127,11 +159,11 @@ void ecdsa_signature(mpz_t r, mpz_t s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t
 
         mpz_import(k, sizeof(buffer), 1, sizeof(buffer[0]), 0, 0, buffer);
         mpz_mod(k, k, q);
-    } while (mpz_cmp_ui(k, 1) < 0)
+    } while (mpz_cmp_ui(k, 1) < 0);
 
     R = point_multiplication(a, b, p, G, k);
 
-    mpz_set(r, R.x);
+    mpz_set(*r, R.x);
 
     if (mpz_invert(k_inv, k, q) == 0) {
         perror("K has no modular inverse\n");
@@ -139,17 +171,17 @@ void ecdsa_signature(mpz_t r, mpz_t s, mpz_t d, mpz_t m, mpz_t p, mpz_t a, mpz_t
         return;
     }
 
-    mpz_mul(temp, d, r);
+    mpz_mul(temp, d, *r);
     mpz_add(temp, temp, m);
     mpz_mod(temp, temp, q);
 
-    mpz_mul(s, temp, k_inv);
-    mpz_mod(s, s, q);
+    mpz_mul(*s, temp, k_inv);
+    mpz_mod(*s, *s, q);
 
     mpz_clears(k, k_inv, R.x, R.y, temp, NULL);
 }
 
-bool ecdsa_verification(mpz_t r, mpz_t s, mpz_t m, char *filename) {
+bool ecdsa_verification(mpz_t *r, mpz_t *s, mpz_t m, char *filename) {
     mpz_t w, aux_1, aux_2, p, a, b, q;
     ec_point G, B, P, P_temp_1, P_temp_2;
 
@@ -162,7 +194,7 @@ bool ecdsa_verification(mpz_t r, mpz_t s, mpz_t m, char *filename) {
         return false;
     }
 
-    if (gmp_fscanf(file, "p = %Zd, a = %Zd, b = %Zd, q = %Zd, G = (%Zd:%Zd:%d), B = (%Zd:%Zd:%d)\n", p, a, b, q, G.x, G.y, G.z, B.x, B.y, B.z) != 10) {
+    if (gmp_fscanf(file, "p = %Zd, a = %Zd, b = %Zd, q = %Zd, G = (%Zd:%Zd:1), B = (%Zd:%Zd:1)\n", p, a, b, q, G.x, G.y, B.x, B.y) != 8) {
         perror("Error reading from public key file\n");
         fclose(file);
         mpz_clears(w, aux_1, aux_2, p, a, b, q, G.x, G.y, B.x, B.y, P.x, P.y, P_temp_1.x, P_temp_1.y, P_temp_2.x, P_temp_2.y, NULL);
@@ -171,7 +203,10 @@ bool ecdsa_verification(mpz_t r, mpz_t s, mpz_t m, char *filename) {
 
     fclose(file);
 
-    if (mpz_invert(w, s, q) == 0) {
+    G.z = 1;
+    B.z = 1;
+
+    if (mpz_invert(w, *s, q) == 0) {
         perror("S has no modular inverse\n");
         mpz_clears(w, aux_1, aux_2, p, a, b, q, G.x, G.y, B.x, B.y, P.x, P.y, P_temp_1.x, P_temp_1.y, P_temp_2.x, P_temp_2.y, NULL);
         return false;
@@ -180,17 +215,17 @@ bool ecdsa_verification(mpz_t r, mpz_t s, mpz_t m, char *filename) {
     mpz_mul(aux_1, w, m);
     mpz_mod(aux_1, aux_1, q);
 
-    mpz_mul(aux_2, w, r);
+    mpz_mul(aux_2, w, *r);
     mpz_mod(aux_2, aux_2, q);
-   
 
-    P_temp_1 = point_multiplication(a, b, p, G, aux_1)
-    P_temp_2 = point_multiplication(a, b, p, B, aux_2)
-    P = point_addition(a, b, p, P_temp_1, P_temp_2);
 
-    bool is_valid = (mpz_cmp(P.x, r) == 0);
+    P_temp_1 = point_multiplication(a, b, p, &G, aux_1);
+    P_temp_2 = point_multiplication(a, b, p, &B, aux_2);
+    P = point_addition(a, b, p, &P_temp_1, &P_temp_2);
+
+    bool is_valid = (mpz_cmp(P.x, *r) == 0);
 
     mpz_clears(w, aux_1, aux_2, p, a, b, q, G.x, G.y, B.x, B.y, P.x, P.y, P_temp_1.x, P_temp_1.y, P_temp_2.x, P_temp_2.y, NULL);
-    
+
     return is_valid;
 }
